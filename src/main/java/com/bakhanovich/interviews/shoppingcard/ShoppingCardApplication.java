d875f107814a70f86104906bad28806757599e2f
Ihar <ihar.bakhanovich@gmail.com>
2022-09-22 20:31:54 +0300

    initialising the project

---
+++ src/main/java/com/bakhanovich/interviews/shoppingcard/ShoppingCardApplication.java
@@ -0,0 +1,13 @@
+package com.bakhanovich.interviews.shoppingcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bakhanovich/interviews/shoppingcard/ShoppingCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakhanovich.interviews.shoppingcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
